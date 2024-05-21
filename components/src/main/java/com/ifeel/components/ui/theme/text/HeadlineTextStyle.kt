83d1c04022b2405cc76e989312e414bb571b0103
RafaGonP <rafael.gonzalez.peces@gmail.com>
2024-05-21 16:54:57 +0200

    RGP Added H3MediumRecoleta to HeadlineTextStyle

--- components/src/main/java/com/ifeel/components/ui/theme/text/HeadlineTextStyle.kt
+++ components/src/main/java/com/ifeel/components/ui/theme/text/HeadlineTextStyle.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import com.ifeel.components.ui.theme.color_text_600
+import com.ifeel.components.ui.theme.recoletaFontFamily
 import com.ifeel.components.ui.theme.sofiaProFontFamily
 
 /**
@@ -27,6 +28,13 @@ sealed class HeadlineTextStyle(
         lineHeight = 36.sp
     )
 
+    data object H3MediumRecoleta : HeadlineTextStyle(
+        color = color_text_600,
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp,
+        fontFamily = recoletaFontFamily,
+        lineHeight = 28.sp
+    )
 
 }
 
