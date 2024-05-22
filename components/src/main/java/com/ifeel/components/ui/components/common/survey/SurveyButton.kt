57336cc8470e5f39616a9c38ce88d720f5a2cb77
GermanBustamante <german@ifeelonline.com>
2024-05-22 11:05:13 +0200

    [IF-4548] Unified Survey with multiple and single options in one composable and deleted classes that can be replaced with Pair<A,B>

--- components/src/main/java/com/ifeel/components/ui/components/common/survey/SurveyButton.kt
+++ components/src/main/java/com/ifeel/components/ui/components/common/survey/SurveyButton.kt
@@ -3,6 +3,7 @@ package com.ifeel.components.ui.components.common.survey
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,6 +19,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.ifeel.components.ui.theme.IfeelComponentsTheme
 import com.ifeel.components.ui.theme.color_brand_primary_400
@@ -25,36 +27,7 @@ import com.ifeel.components.ui.theme.color_text_700
 import com.ifeel.components.ui.theme.text.BodyTextStyle
 
 @Composable
-internal fun SurveySingleOptionButton(
-    text: String,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    val interactionSource = remember { MutableInteractionSource() }
-    val isPressed by interactionSource.collectIsPressedAsState()
-
-    Button(
-        onClick = onClick,
-        modifier = modifier
-            .height(64.dp)
-            .fillMaxHeight(),
-        shape = RoundedCornerShape(12.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = if (isPressed) color_brand_primary_400 else Color.White,
-        ),
-        interactionSource = interactionSource,
-    ) {
-        Text(
-            text = text,
-            style = BodyTextStyle.Body16Regular.toTextStyle().copy(
-                color = if (isPressed) Color.White else color_text_700
-            )
-        )
-    }
-}
-
-@Composable
-internal fun SurveyMultipleOptionButton(
+internal fun SurveyOptionButton(
     text: String,
     selected: Boolean,
     onSelectedChange: (Boolean) -> Unit,
@@ -65,9 +38,7 @@ internal fun SurveyMultipleOptionButton(
 
     Button(
         onClick = { onSelectedChange(!selected) },
-        modifier = modifier
-            .height(80.dp)
-            .fillMaxHeight(),
+        modifier = modifier.fillMaxWidth(),
         shape = RoundedCornerShape(12.dp),
         colors = ButtonDefaults.buttonColors(
             containerColor = if (isPressed || selected) color_brand_primary_400 else Color.White,
@@ -90,11 +61,11 @@ private fun SurveyButtonPreview() {
         mutableStateOf(false)
     }
     IfeelComponentsTheme {
-        SurveyMultipleOptionButton(
+        SurveyOptionButton(
             text = "Test",
             selected = selected,
             onSelectedChange = { selected = it },
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier.padding(8.dp).height(64.dp)
         )
     }
 }
