42bc91ccebe4ce226f9768d8e88cb7b071753555
GermanBustamante <germanbustamante.trabajo@gmail.com>
2024-06-13 18:06:29 +0200

    Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -45,11 +51,22 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.3.0")
+
     //Components library
-//    implementation("com.github.GermanIfeelDev:Ifeel-Components:0.0.1")
+    implementation("com.github.GermanIfeelDev:Ifeel-Components:0.0.1")
 }
--- app/src/main/java/com/ifeel/ifeelcomponents/MainActivity.kt
+++ app/src/main/java/com/ifeel/ifeelcomponents/MainActivity.kt
@@ -1,8 +1,165 @@
 package com.ifeel.ifeelcomponents
 
-import android.app.Activity
-import androidx.core.app.ComponentActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ifeel.components.R
+import com.ifeel.components.ui.components.common.textfield.PasswordTextField
+import com.ifeel.components.ui.components.generic.button.ButtonType
+import com.ifeel.components.ui.components.generic.button.PrimaryButton
+import com.ifeel.components.ui.components.generic.spinner.Spinner
+import com.ifeel.components.ui.components.generic.textfield.EditableTextField
+import com.ifeel.components.ui.components.generic.textfield.OutlinedTextField
+import com.ifeel.components.ui.theme.IfeelComponentsTheme
+import com.ifeel.components.ui.theme.recoletaFontFamily
+import com.ifeel.components.ui.theme.sofiaProFontFamily
+import com.ifeel.components.ui.theme.text.HeadlineTextStyle
 
-class MainActivity : Activity() {
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IfeelComponentsTheme {
+                var showTextComponents by rememberSaveable { mutableStateOf(false) }
+                var showInputComponents by rememberSaveable { mutableStateOf(false) }
+                var checkboxChecked by rememberSaveable { mutableStateOf(false) }
+
+                Column(modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .horizontalScroll(
+                        rememberScrollState()
+                    )) {
+                    Row {
+                        PrimaryButton(
+                            text = "See some text with other styles",
+                            onClick = { showTextComponents = !showTextComponents })
+                        PrimaryButton(
+                            text = "Input components",
+                            onClick = { showInputComponents = !showInputComponents },
+                            buttonType = ButtonType.SECONDARY,
+                            contentAlignment = Alignment.End,
+                            iconResource = R.drawable.supporting_text_info_ic
+                        )
+
+                        Spinner(
+                            title = "Spinner",
+                            hint = "Choose option",
+                            options = arrayOf("Option 1", "Option 2", "Option 3"),
+                            onOptionSelected = {}
+                        )
+
+                        Checkbox(checked = checkboxChecked, onCheckedChange = {checkboxChecked = it})
+                    }
+                    if (showTextComponents) {
+                        TextComponents()
+                    }
+
+                    if (showInputComponents) {
+                        InputsComponents()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TextComponents(modifier: Modifier = Modifier) {
+    Column {
+        Divider(modifier = Modifier.padding(vertical = 8.dp))
+        Text(
+            text = "We can create text styles in 3 different ways.\n" +
+                    "1. Using the objects in the library, which are based on the Ifeel Figma System\n" +
+                    "2. Taking the above, and customizing it with a copy()\n" +
+                    "3. Creating a TextStyle from 0 (This would be the most extreme case, the best thing is that if that TextStyle differs a lot from all of them, and is used in several places, the developer is asked to put it in the library)"
+        )
+        Text(
+            text = "1. H2 SemiBold With Default Object",
+            style = HeadlineTextStyle.H2SemiBold.toTextStyle(),
+            modifier = Modifier.padding(top = 10.dp)
+        )
+        Text(
+            text = "2. H2 Semibold With Copy (Changed FontFamily to Recoleta and Font Weight to Medium)",
+            style = HeadlineTextStyle.H2SemiBold.toTextStyle().copy(
+                fontFamily = recoletaFontFamily, fontWeight = FontWeight.Medium
+            ),
+            modifier = Modifier.padding(top = 10.dp)
+        )
+        Text(
+            text = "Text Style fully customized",
+            style = TextStyle(
+                fontFamily = sofiaProFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontStyle = FontStyle.Italic,
+                fontSize = 20.sp
+            ),
+        )
+        Divider(modifier = Modifier.padding(vertical = 8.dp))
+    }
+}
+
+@Composable
+fun InputsComponents(modifier: Modifier = Modifier) {
+    var inputValue by rememberSaveable { mutableStateOf("") }
+
+    Column {
+        Divider(modifier = Modifier.padding(vertical = 8.dp))
+        PasswordTextField(
+            value = inputValue,
+            onValueChange = { inputValue = it },
+            supportingText = "Supporting text"
+        )
+        Divider(modifier = Modifier.padding(vertical = 8.dp))
+        OutlinedTextField(
+            value = inputValue,
+            onValueChange = { inputValue = it },
+            label = "Email"
+        )
+        Divider(modifier = Modifier.padding(vertical = 8.dp))
+        EditableTextField(
+            value = inputValue,
+            onValueChange = { inputValue = it },
+            isError = true,
+            errorMessage = "Error message"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextComponentsPreview() {
+    IfeelComponentsTheme {
+        TextComponents()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextFieldComponentsPreview() {
+    IfeelComponentsTheme {
+        InputsComponents()
+    }
 }
 
