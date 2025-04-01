da628622f9fae58565fb25f0c4964816f6f393e3
German De Bustamante Conde <germanbustamante.trabajo@gmail.com>
2025-04-01 13:43:32 +0200

    [INN-3127] Delete unused fonts

--- components/src/main/java/com/ifeel/components/ui/theme/Font.kt
+++ components/src/main/java/com/ifeel/components/ui/theme/Font.kt
@@ -2,7 +2,6 @@ package com.ifeel.components.ui.theme
 
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import com.ifeel.components.R
 
@@ -10,13 +9,8 @@ val sofiaProFontFamily = FontFamily(
     Font(R.font.sofia_pro_semibold, FontWeight.SemiBold),
     Font(R.font.sofia_pro_medium, FontWeight.Medium),
     Font(R.font.sofia_pro_regular, FontWeight.Normal),
-    Font(R.font.sofia_pro_semibold_italic, FontWeight.SemiBold, FontStyle.Italic),
-    Font(R.font.sofia_pro_medium_italic, FontWeight.Medium, FontStyle.Italic),
-    Font(R.font.sofia_pro_regular_italic, FontWeight.Normal, FontStyle.Italic),
 )
 
 val recoletaFontFamily = FontFamily(
-    Font(R.font.recoleta_semibold, FontWeight.SemiBold),
     Font(R.font.recoleta_medium, FontWeight.Medium),
-    Font(R.font.recoleta_regular, FontWeight.Normal),
 )
